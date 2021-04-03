3b51b6222062bf95ef6a4674c156f6d83aedc2bd
markosanchez800 <markosanchez800@gmail.com>
2021-04-03 16:49:47 -0700

    site pulls accurate data from openweather API

---
+++ script.js
@@ -0,0 +1,51 @@
+var cityForm = document.querySelector("#city-form");
+var cityInput = document.querySelector("#city");
+
+
+var formSubmit = function(event){
+    event.preventDefault();
+    var city = cityInput.value.trim();
+    if (city){
+        getCityWeather(city);
+    }
+}
+
+
+
+
+// Read up on modems and replace alerts!!!!!
+var getCityWeather = function(city){
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=c8f225cee6b1a2085c93026298f7c5a4';
+
+    fetch(apiUrl)
+    .then(function(response){
+        if (response.ok) {
+            response.json().then(function(data){
+                displayWeather(data,city);
+                console.log(data,city);
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+    })
+    .catch(function(error){
+        alert('Unable to connect to OpenWeather');
+    });
+};
+
+var displayWeather = function(data,city){
+    var showName = document.createElement('h1');
+    var tempy = document.createElement('h2');
+    var humidity = document.createElement('h2');
+    var windy = document.createElement('h2');
+    //var uvIndo = document.createElement('h2');
+    showName = data.name;
+    tempy = data.main.temp;
+    humidity = data.main.humidity;
+    windy = data.wind.speed;
+    //uvIndo = data.
+    console.log(showName);
+}
+
+
+cityForm.addEventListener('submit',formSubmit);
