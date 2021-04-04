2594002f9d68335b1ef187f0e7fbdd718993c672
markosanchez800 <markosanchez800@gmail.com>
2021-04-03 18:27:15 -0700

    clear labeling for values given to user and site also now pulls correct data for five day forecast

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var cityForm = document.querySelector("#city-form");
 var cityInput = document.querySelector("#city");
+var weatherBox = document.querySelector("#weather-container");
 
 
 var formSubmit = function(event){
@@ -7,6 +8,7 @@ var formSubmit = function(event){
     var city = cityInput.value.trim();
     if (city){
         getCityWeather(city);
+        getFiveDay(city);
     }
 }
 
@@ -33,21 +35,52 @@ var getCityWeather = function(city){
     });
 };
 
+var getFiveDay = function(city){
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=c8f225cee6b1a2085c93026298f7c5a4&cnt=5';
+
+    fetch(apiUrl)
+    .then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                //displayFiveDay(data,city);
+                console.log(data,city);
+            });
+        } else { 
+            alert('Error: ' + response.statusText);
+        }
+    })
+    .catch(function(error){
+        alert('Unable to connect to OpenWeather');
+    });
+};
+
 var displayWeather = function(data,city){
+    var weatherList = document.createElement('a');
     var showName = document.createElement('h1');
-    var tempy = document.createElement('h2');
-    var humidity = document.createElement('h2');
-    var windy = document.createElement('h2');
+    var typePic = document.createElement('img');
+    var tempy = document.createElement('h1');
+    var humidity = document.createElement('h1');
+    var windy = document.createElement('h1');
     //var uvIndo = document.createElement('h2');
-    showName = data.name;
-    tempy = (data.main.temp-273.15)*9/5+32;
-    humidity = data.main.humidity;
-    windy = data.wind.speed;
+    showName.innerHTML = data.name;
+    typePic.img = data.weather.icon;
+    tempy.innerHTML = 'Temperature: ' + (data.main.temp-273.15)*9/5+32 + ' F°';
+    humidity.innerHTML = 'Humidity: ' + data.main.humidity + '%';
+    windy.innerHTML = 'Wind Speed: ' + data.wind.speed + ' mph';
     //uvIndo = data.
     console.log(showName);
     console.log(tempy);
     console.log(humidity);
     console.log(windy);
+
+    weatherList.classList = 'list-item flex-row justify-space-between align-center';
+    weatherList.appendChild(showName);
+    showName.append(typePic);
+    showName.appendChild(tempy);
+    tempy.appendChild(humidity);
+    humidity.appendChild(windy);
+    weatherBox.appendChild(weatherList);
+    
 }
 
 
